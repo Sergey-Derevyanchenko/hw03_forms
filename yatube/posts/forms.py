0c5689d8c7da0ade433d8d542f5c5df381565129
Сергей Деревянченко <sergey.derevyanchenko@yandex.ru>
2022-04-11 10:54:11 +0300

    Sprint 4. Iteration 1.

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea, required=True)
-    
+
     class Meta:
         model = Post
         fields = ('text', 'group',)
